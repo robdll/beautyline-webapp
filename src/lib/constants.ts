29637e6395a4f48b432e8a68549cf410e80bb5c3
Roberto Di Lillo <dilillo.roberto@gmail.com>
2025-12-18 16:17:00 -0300

    adds constant

---
+++ src/lib/constants.ts
@@ -0,0 +1,18 @@
+export const HOME_COURSE_CARDS = [
+  {
+    title: 'Corsi Unghie',
+    imageSrc: '/images/card-1.jpg',
+    href: '/corsi',
+  },
+  {
+    title: 'Corsi Occhi',
+    imageSrc: '/images/card-2.png',
+    href: '/corsi',
+  },
+  {
+    title: 'Percorsi Master',
+    imageSrc: '/images/card-3.png',
+    href: '/corsi',
+  },
+] as const;
+

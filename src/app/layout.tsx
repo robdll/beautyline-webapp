6184f9cf803c77dec6182c33a8d0a45afec05802
Roberto Di Lillo <dilillo.roberto@gmail.com>
2026-04-28 11:00:19 -0300

    feat adds SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter, Raleway } from "next/font/google";
 import "@/styles/global.css";
 import { Navbar } from "@/components/shared/Navbar";
@@ -65,6 +66,7 @@ export default function RootLayout({
           </CartProvider>
         </AuthProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
